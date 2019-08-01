6b07b6b6486d7f6f886fdec9b2059b202c534db8
nikulinsergey <niku7in.s.s@gmail.com>
2019-08-01 17:10:16 +0300

    Updates

---
+++ precache-manifest.8f5576f58b831f41ae8dc3ca8cfb8274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccaabddb61f52c7379a27873e1ad98f",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "07f86a16a26e92967278",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "be6ccef193cfc8253023",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.be9a85fd.chunk.css"
+  },
+  {
+    "revision": "07f86a16a26e92967278",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.563ea6ef.chunk.js"
+  },
+  {
+    "revision": "be6ccef193cfc8253023",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.40b07b36.chunk.js"
+  },
+  {
+    "revision": "671ff8e999e03896268d",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.0f0e45e8.js"
+  }
+]);
